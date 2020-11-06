c73040a789fa59223705690955b069110b954b6a
Alvaro Sosa <alvsosa@alumno.unlam.edu.ar>
2020-11-06 11:39:42 -0300

    agregue clase auxiliar, source test y clase sistemaTest

---
+++ src/sistemaEscolar/Auxiliar.java
@@ -0,0 +1,16 @@
+package sistemaEscolar;
+
+public class Auxiliar extends Persona {
+
+	public Auxiliar(String nombre, String apellido, Integer dni) {
+		super(nombre, apellido, dni);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void verDesempeño(String desempeño) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
